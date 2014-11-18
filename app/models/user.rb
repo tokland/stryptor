0ff9a41dfb3551152548202a03424494fa41bbf4
Arnau Sanchez <pyarnau@gmail.com>
2014-11-18 17:37:52 +0100

    refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   has_many :transcripts, inverse_of: :user, dependent: :nullify
   
   def self.create_with_omniauth!(auth)
-    attrs1 = auth.slice(:provider, :uid)
-    attrs2 = auth.fetch("info", {}).slice(:email, :name)
-    User.create!(attrs1.merge(attrs2).to_h)
+    provider_attrs = auth.slice(:provider, :uid)
+    info_attrs = auth.fetch("info", {}).slice(:email, :name)
+    User.create!(provider_attrs.merge(info_attrs).to_h)
   end
 end
